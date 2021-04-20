5bd382e07a79dec56e233ff8d979ed1a007e3f82
adamgajzlerowicz <nelf86@gmail.com>
2021-04-20 15:34:38 +0200

    Added data fetching poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+import useWebSocket, {ReadyState} from "react-use-websocket";
+
 import './App.css';
 
-function App() {
+const getDataMessage = {"event":"subscribe","feed":"book_ui_1","product_ids":["PI_XBTUSD"]}
+
+type ApiData = {}
+
+const parseApiData = (data: string): ApiData | null => {
+  try{
+      return JSON.parse(data)
+  }catch(e) {
+    return null
+  }
+}
+
+const App = () => {
+  const [socketUrl] = React.useState('wss://www.cryptofacilities.com/ws/v1');
+
+  const {
+    sendJsonMessage,
+    lastMessage,
+    readyState,
+  } = useWebSocket(socketUrl, { onOpen: () => {
+      sendJsonMessage(getDataMessage)
+    }});
+
+  if (readyState === ReadyState.CONNECTING) {
+    return <div>loading</div>
+  }
+
+  if (readyState !== ReadyState.OPEN) {
+    return <div>loading</div>
+  }
+
+  const data = parseApiData(lastMessage?.data)
+  console.log(data);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
